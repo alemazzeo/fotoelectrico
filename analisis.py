b16ffa9b2d4454534c73553b6b45d13ce65dcec7
Mazzeo Alejandro <ale.exactas@gmail.com>
2017-10-15 22:27:44 -0300

    Trabajando en el analisis.

---
+++ analisis.py
@@ -0,0 +1,86 @@
+# -*- coding: utf-8 -*-
+
+import matplotlib
+matplotlib.use('Qt5Agg')
+
+import numpy as np
+from scipy import signal
+from scipy.optimize import curve_fit
+import matplotlib.pyplot as plt
+
+from datos import mediciones, espectros, ventana_espectros
+
+
+def sol_monocromatica(v_ret, v_ret0, m, offset=1.5e-6):
+    return (v_ret - v_ret0) * m * (v_ret > v_ret0) + offset
+
+
+def ajusta_recta(v_ret, v_foto, x0):
+    x = v_ret[v_ret > x0]
+    y = v_foto[v_ret > x0]
+    a, b = np.polyfit(x, y, 1)
+    return a, b
+
+
+def ajusta_monocromatica(v_ret, v_foto, x0, p=1):
+    a, b = ajusta_recta(v_ret, v_foto, x0)
+
+    params = np.asarray([(1.6e-6 - b) / a, a, 1.6e-6])
+    upper = np.sign(params) * np.abs(params) * (1 + p)
+    lower = np.sign(params) * np.abs(params) * (1 - p)
+
+    popt, pcov = curve_fit(sol_monocromatica, v_ret, v_foto,
+                           p0=params, bounds=(lower, upper))
+
+    fig, ax = plt.subplots(1)
+    ax.plot(v_ret, v_foto)
+    # ax.plot(v_ret, a * v_ret + b)
+    ax.plot(v_ret, sol_monocromatica(v_ret, *popt))
+
+    return params, upper, lower, popt
+
+
+def ver_medicion(nombre):
+    v_ret, v_foto = mediciones[nombre]
+    fig, ax = plt.subplots(1)
+    ax.plot(v_ret, v_foto)
+
+
+def ver_espectro(nombre):
+    long_onda, intensidad = espectros[nombre]
+    fig, ax = plt.subplots(1)
+    ax.plot(long_onda, intensidad)
+    ax.set_xlim(ventana_espectros[nombre])
+
+
+plt.ion()
+
+
+def ver_juntos(nombre, espectro=''):
+    fig, ax = plt.subplots(2)
+    v_ret, v_foto = mediciones[nombre]
+    long_onda, intensidad = espectros[nombre + espectro]
+
+    ax[0].plot(v_ret, v_foto)
+    ax[0].plot(v_ret, sol_monocromatica(v_ret, 0.7, 0.75e-5) + v_foto[0])
+    ax[1].plot(long_onda, intensidad)
+
+
+def ver_led_blanco():
+    fig, ax = plt.subplots(2)
+    for i in range(12, 21):
+        v_ret, v_foto = mediciones[str(i)]
+        long_onda, intensidad = espectros[str(i)]
+
+        ax[0].plot(v_ret, v_foto)
+        ax[1].plot(long_onda, intensidad)
+        # ax[1].set_xlim(ventana_espectros[str(i)])
+
+
+def ver_barrido_led():
+    fig, ax = plt.subplots(2)
+    for i, letra in enumerate(['A', 'B', 'C', 'D', 'E']):
+        v_ret, v_foto = mediciones['10']
+        long_onda, intensidad = espectros['10' + letra]
+        ax[0].plot(v_ret, v_foto[i])
+        ax[1].plot(long_onda, intensidad)
--- datos.py
+++ datos.py
@@ -44,7 +44,12 @@ for path, subdirs, files in os.walk('.'):
                 V = np.linspace(-2.0, 2.0, 101)
                 x = np.asarray([corrige_fase(X[i], Y[i]) for i in range(3)])
 
-            elif m >= 12:
+            elif m in [12, 13]:
+                X, Y = np.load(ruta)
+                V = np.linspace(-2.0, 2.0, 201)
+                x = corrige_fase(X, Y)
+
+            elif m >= 14:
                 X, Y = np.load(ruta)
                 V = np.linspace(-2.0, 2.0, 101)
                 x = corrige_fase(X, Y)
@@ -58,3 +63,37 @@ for path, subdirs, files in os.walk('.'):
                 datos = [dato for dato in f]
             espectro = np.loadtxt(datos[33:-2], delimiter=',', comments='#')
             espectros.update({nombre[8:]: espectro.T})
+
+ventana_espectros = {'01A': [455, 480],
+                     '01B': [455, 480],
+                     '02A': [455, 480],
+                     '02B': [455, 480],
+                     '03A': [455, 480],
+                     '04B': [455, 480],
+                     '04A': [455, 480],
+                     '05B': [455, 480],
+                     '05A': [455, 480],
+                     '06B': [440, 480],
+                     '06A': [440, 480],
+                     '07A': [470, 510],
+                     '07B': [470, 510],
+                     '08A': [455, 485],
+                     '08B': [455, 485],
+                     '09': [450, 490],
+                     '10A': [455, 485],
+                     '10B': [455, 485],
+                     '10C': [455, 485],
+                     '10D': [455, 485],
+                     '10E': [455, 485],
+                     '11A': [580, 660],
+                     '11B': [580, 660],
+                     '11C': [580, 660],
+                     '12': [580, 680],
+                     '13': [580, 660],
+                     '14': [420, 470],
+                     '15': [440, 500],
+                     '16': [460, 560],
+                     '17': [500, 600],
+                     '18': [560, 700],
+                     '19': [600, 750],
+                     '20': [620, 800]}
